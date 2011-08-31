53dffcc0e39fe89e56a4ac68d161a22c6bc17ce2
J.W.Marsden <j.w.marsden@gmail.com>
2011-08-31 19:49:37 +0930

    Style update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,23 @@ module ApplicationHelper
 
      content_tag("div", links, :id => "breadcrumb")
    end  
+   
+   	def title
+		base_title = "Plural.cc"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} - #{@title}"
+		end
+	end
+
+	
+	def heading
+		base_heading = "I aint afraid of no ghost"
+		if @heading.nil?
+			base_heading
+		else
+			@heading
+		end
+	end
 end
